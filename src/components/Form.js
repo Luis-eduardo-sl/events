7652a273887a62e450915b42b58802b6135956fe
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-19 17:14:46 -0300

    using useState on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 function Form (){
     function cadastrarUsuario(e){
         e.preventDefault();
+        console.log(nome);
         console.log("Usuario cadastrado");
-    }
+    };
+
+    const[nome, setNome] = useState()
+
     return(
         <div>
             <h1>Cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"></input>
+                    <input
+                     type="text" 
+                     id="nome"
+                     name="nome"
+                     value={nome}
+                     placeholder="Digite seu nome"
+                     onChange={(e)=> setNome(e.target.value)}
+                     ></input>
                 </div>
                 <div>
-                    <input type="email" placeholder="Digite seu email"></input>
+                    <input 
+                    type="email" 
+                    id="email"
+                    name="email"
+                    placeholder="Digite seu email"></input>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"></input>
